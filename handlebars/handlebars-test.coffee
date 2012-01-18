7a1ce9a2a5919a79740b0a78bcec4a8c5685dd9c
David Elentok <3david@gmail.com>
2012-01-19 00:47:45 +0200

    handlebars: added master+partial templates

--- handlebars/handlebars-test.coffee
+++ handlebars/handlebars-test.coffee
@@ -1,3 +1,8 @@
 # Single template
 single_template = Handlebars.compile $('#turtles-single-template').html()
 $('#single').html single_template(data)
+
+# Master and partial templates
+master_template = Handlebars.compile $('#turtles-master-template').html()
+Handlebars.registerPartial 'turtle', $('#turtle-partial-template').html()
+$('#master-and-partial').html master_template(data)
--- handlebars/handlebars-test.js
+++ handlebars/handlebars-test.js
@@ -1,5 +1,8 @@
 (function() {
-  var single_template;
+  var master_template, single_template;
   single_template = Handlebars.compile($('#turtles-single-template').html());
   $('#single').html(single_template(data));
+  master_template = Handlebars.compile($('#turtles-master-template').html());
+  Handlebars.registerPartial('turtle', $('#turtle-partial-template').html());
+  $('#master-and-partial').html(master_template(data));
 }).call(this);
